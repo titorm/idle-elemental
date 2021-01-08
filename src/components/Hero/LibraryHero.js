38e9731c780ce09c1ffe1f1d05dcd82b22d9dec6
Phellipe Perin <phellipe.perin@gmail.com>
2021-01-08 17:00:48 +0000

    image test

--- src/components/Hero/LibraryHero.js
+++ src/components/Hero/LibraryHero.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import styles from './LibraryHeroStyles';
 
 function LibraryHero({ hero }) {
     return (
         <View style={styles.item}>
+            <Image
+                style={styles.image}
+                source={require('../../assets/images/foxy.png')}
+            />
             <Text>{hero.gameInfo.basicInfo.name} - {hero.gameInfo.category.rarity}</Text>
             <Text>{hero.playerInfo.stars} stars</Text>
             <Text>{hero.playerInfo.ascension} ascension</Text>
--- src/components/Hero/LibraryHeroStyles.js
+++ src/components/Hero/LibraryHeroStyles.js
@@ -4,6 +4,10 @@ const styles = StyleSheet.create({
     item: {
         marginBottom: 16,
     },
+    image: {
+        height: 48,
+        width: 48,
+    },
 });
 
 export default styles;
